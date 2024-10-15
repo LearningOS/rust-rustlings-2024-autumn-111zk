22dbe4a4a5047eb1a590d2b389b0cd3d9bb70722
Your Name <you@example.com>
2024-10-15 22:53:16 +0800

    solve53

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -16,7 +16,10 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
+        if value < 1 {
+            return Err(CreationError::Negative);
+        }
+
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,9 +18,6 @@
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
 //
-// No hints this time!
-
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -31,12 +28,15 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let transformed_string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(count) => format!("{}{}", &string, "bar".repeat(*count)),
+            };
+            output.push(transformed_string);
         }
         output
     }
@@ -44,8 +44,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
